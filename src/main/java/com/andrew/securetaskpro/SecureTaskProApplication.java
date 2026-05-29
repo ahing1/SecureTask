8e0624c98f1a240d6a7fa2cce0c5c79ecb444db1
Andrew Hing <andrew@andrews-mbp.lan>
2026-05-28 20:17:53 -0400

    initial commit

---
+++ src/main/java/com/andrew/securetaskpro/SecureTaskProApplication.java
@@ -0,0 +1,13 @@
+package com.andrew.securetaskpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureTaskProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureTaskProApplication.class, args);
+	}
+
+}
